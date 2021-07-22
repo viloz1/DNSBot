2eae9ace9e3f536d6633eeeb2abc03ccccb5dc8f
viloz1 <simonjohansson312@gmail.com>
2021-07-22 16:04:16 +0200

    More deploy things

--- bot.py
+++ bot.py
@@ -3,26 +3,27 @@ from command.__init__ import *
 from events.__init__ import *
 from command.music.__init__ import *
 import discord
+import sys
 
-def main(type):
-    f = open(os.path.abspath(os.getcwd()) + '/config.json')
-    config = json.load(f)
-    f.close()
+f = open(os.path.abspath(os.getcwd()) + '/config.json')
+config = json.load(f)
+f.close()
 
-    load_dotenv()
-    ENV_VAR = os.environ
-    TOKEN = ENV_VAR[(config[type]["token"])]
+type = sys.argv[1]
+load_dotenv()
+ENV_VAR = os.environ
+TOKEN = ENV_VAR[(config[type]["token"])]
 
-    intents = discord.Intents.default()
-    intents.members = True
-    client = commands.Bot(command_prefix='!', intents=intents)
+intents = discord.Intents.default()
+intents.members = True
+client = commands.Bot(command_prefix='!', intents=intents)
 
-    events = Events(client, config[type], ENV_VAR)
-    command = Commands(client, config[type], ENV_VAR)
-    music = Music(client, config, ENV_VAR)
+events = Events(client, config[type], ENV_VAR)
+command = Commands(client, config[type], ENV_VAR)
+music = Music(client, config, ENV_VAR)
 
-    client.add_cog(events)
-    client.add_cog(command)
-    client.add_cog(music)
+client.add_cog(events)
+client.add_cog(command)
+client.add_cog(music)
 
-    client.run(TOKEN)
+client.run(TOKEN)
